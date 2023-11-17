53c61d21484d5926ec0364b2d540fedfef786a7c
Guyane <claretdamien@gmail.com>
2023-11-17 20:17:32 +0100

    Fixed bugs and started 2FA

--- src/components/NOTPVerify/NOTPSVerify.tsx
+++ src/components/NOTPVerify/NOTPSVerify.tsx
@@ -3,7 +3,6 @@ import base32 from "thirty-two";
 
 export default function NOTPSVerify(token: string) {
     const key = "12345";
-
     const base32Encoded = base32.encode(key).toString().replace(/=/g, "");
 
     const login = notp.totp.verify(token, base32Encoded);
