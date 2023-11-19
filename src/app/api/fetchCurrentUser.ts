86de65ea47e236b00581a9f416f5c234924e2995
Guyane <claretdamien@gmail.com>
2023-11-19 15:43:07 +0100

    Production

--- src/app/api/fetchCurrentUser.ts
+++ src/app/api/fetchCurrentUser.ts
@@ -2,7 +2,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./auth/[...nextauth]/route";
 import { prisma } from "../../../lib/prisma";
-import CheckSession from "../../../lib/CheckSession.ts";
+import CheckSession from "../../../lib/CheckSession";
 
 export default async function fetchCurrentUser(
     id: undefined | string = undefined
